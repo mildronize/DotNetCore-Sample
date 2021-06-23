7e0e7449f5bf680aa22ce1e9d9a9f3170d841391
Thada Wangthammang <mildronize@gmail.com>
2021-06-23 17:54:35 +0700

    Run `aspnet-codegenerator identity` to customize identity in unit 4

--- Microsoft.Learn.SecureAspnetCoreIdentity/final/ContosoPets.Ui/Areas/Identity/Data/ContosoPetsAuth.cs
+++ Microsoft.Learn.SecureAspnetCoreIdentity/final/ContosoPets.Ui/Areas/Identity/Data/ContosoPetsAuth.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoPets.Ui.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ContosoPets.Ui.Areas.Identity.Data
 {
-    public class ContosoPetsAuth : IdentityDbContext<IdentityUser>
+    public class ContosoPetsAuth : IdentityDbContext<ContosoPetsUser>
     {
         public ContosoPetsAuth(DbContextOptions<ContosoPetsAuth> options)
             : base(options)
